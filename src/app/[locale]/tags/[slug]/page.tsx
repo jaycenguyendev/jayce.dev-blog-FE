7d14caf0a7ef6b38577889e3c9d91dd872496b0c
Jayce.Nguyen <jayce.nguyen@avepoint.com>
2024-05-01 11:13:21 +0700

    [Add] create tag item component

--- src/app/[locale]/tags/[slug]/page.tsx
+++ src/app/[locale]/tags/[slug]/page.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
-type Props = {};
+type Props = {
+  params: {
+    slug: string;
+  };
+};
 
 const Tags = (props: Props) => {
   return <div>Tag page</div>;
--- src/components/shared/tags.tsx
+++ src/components/shared/tags.tsx
@@ -1,25 +1,22 @@
 import CustomLink from '@/components/shared/link';
-import { Tag } from '@/types';
+import Tag from '@/components/shared/tag';
+import { Tag as TagType } from '@/types';
 import React from 'react';
 
 type Props = {
   className?: string;
-  tags: Tag[];
+  tags: TagType[];
 };
 
 const Tags = ({ className, tags }: Props) => {
   return (
     <ul className={`flex ${className} flex-wrap`}>
+      <li>
+        <Tag href={`/blog`} slug="All" />
+      </li>
       {tags.map((tag) => (
         <li key={tag.slug}>
-          <CustomLink
-            className="group relative mb-2 mr-2 inline-flex items-center justify-center overflow-hidden rounded-lg bg-gradient-to-br from-cyan-500 to-blue-500 p-0.5 text-sm font-medium text-gray-900 hover:text-white focus:outline-none focus:ring-4 focus:ring-cyan-200 group-hover:from-cyan-500 group-hover:to-blue-500 dark:text-white dark:focus:ring-cyan-800 from-cyan-500 to-blue-500"
-            href={`/tags/${tag.slug}`}
-          >
-            <span className="relative rounded-md bg-white px-5 py-2.5 transition-all duration-75 ease-in group-hover:bg-opacity-0 dark:bg-dark whitespace-nowrap">
-              {tag.slug.toLocaleLowerCase()}
-            </span>
-          </CustomLink>
+          <Tag href={`/tags/${tag.slug}`} slug={tag.slug.toLocaleLowerCase()} />
         </li>
       ))}
     </ul>
